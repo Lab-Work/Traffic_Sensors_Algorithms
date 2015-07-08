a5b029e026eda19ee0fecbf966ef6428da038f58
Fangyu Wu <mianaomate@gmail.com>
2015-07-08 10:11:23 -0500

    Added static threshold method

--- SmartCone/ULTest.py
+++ SmartCone/ULTest.py
@@ -1,6 +1,9 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
+from astroML.plotting import setup_text_plots
+setup_text_plots(fontsize=14, usetex=True) # This will create LaTex style plotting.
 
 '''
 #The raw file needs to be moderately processed to omit text before numerical analysis
@@ -33,23 +36,91 @@ with open('database/ULTest_2.csv','r') as dest:
         laser.append(float(line[4]))
         count.append(float(line[5]))
 
-ultra = np.array(ultra)
-ultra = (ultra - np.average(ultra)) / np.std(ultra)
-
+ultra = np.array(ultra) - np.average(ultra)
 laser = np.array(laser)
-laser = (laser - np.average(laser)) / np.std(laser)
 
-SPK = 0
-for i in range(len(ultra)):
-    if ultra[i] > -2:
-        laser[i] = 0.9
+laserMode = max(laser)
+print laserMode
 
-for i in range(len(ultra)-1):
-    if ultra[i+1] - ultra[i] < -1.5:
-        SPK += 1
-print SPK
+for i in range(len(laser)):
+    if laser[i] > 10:
+        laser[i] = 0
+
+distance = np.array([i**2 + j**2 for i, j in zip(ultra, laser)])
+
 
 plt.figure()
-plt.plot(ultraTime, ultra)
-plt.plot(laserTime, laser)
+plt.plot(ultraTime, -ultra, label='Ultrasonic')
+plt.plot(laserTime, laser, label='Laser')
+plt.xlabel('Machine Time')
+plt.ylabel('Relative Signal Maganitude')
+plt.title('Comparison of Laser and Ultrasonic Sensor Signals 06/25/15')
+plt.legend()
+#plt.plot(ultraTime, distance)
+#plt.show()
+
+
+COUNT = []
+for i in range(len(distance)):
+    if distance[i] > 20:
+        COUNT.append(1)
+    else:
+        COUNT.append(0)
+
+BASKET = []
+TMP = []
+for i in range(len(COUNT)):
+    if i == 0:
+        TMP = [COUNT[i]]
+    if COUNT[i] == TMP[0]:
+        TMP.append(COUNT[i])
+    else:
+        BASKET.append(TMP)
+        TMP = [COUNT[i]]
+
+for SPK in BASKET:
+    if len(SPK) > 2 and SPK[0] == 1:
+        for i in range(len(SPK)):
+            if i != 0:
+                SPK[i] = 0
+    elif SPK[0] == 1:
+        for i in range(len(SPK)):
+            SPK[i] = 0
+#print BASKET
+
+BASKET = np.array([i for SUB in BASKET for i in SUB])
+BASKET = np.cumsum(BASKET)
+
+logTime = []
+logSPK = []
+with open('data/log.txt', 'r') as log:
+    reader = csv.reader(log)
+    reader.next()
+    for line in reader:
+        logTime.append(datetime.strptime(line[0], '%Y-%m-%d %H:%M:%S.%f'))
+        logSPK.append(line[2])
+
+print len(logSPK)
+print max(BASKET)
+
+plt.figure()
+plt.plot(logTime, logSPK)
+plt.xlabel('Time Stamp')
+plt.ylabel('Cumulative Traffic')
+plt.title('Cumulative Traffic Diagram (Ground Truth)')
+
+plt.figure()
+plt.plot(ultraTime[:-1], BASKET)
+plt.xlabel('Machine Time')
+plt.ylabel('Cumulative Traffic')
+plt.title('Computed Cumulative Traffic Diagram')
 plt.show()
+
+
+'''
+counter = 0
+for SPK in  BLASKET:
+    if len(SPK) > 2 and SPK[0] == 1:
+        counter += 1
+        print counter
+'''
