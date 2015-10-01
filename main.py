97d2e90edbec31afee2cced19c7687d46056f8fb
Fangyu Wu <fangyuthegreen@gmail.com>
2015-10-01 01:16:14 -0500

    Added new visualization methods.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ sys.path.append('./tools')
 from _parser import parse
 from crosscorrelation import find_pixels_shifts
 
-print "This is the main function."
+print "Executing MAIN..."
 
 PIR_data, IMUU_reduced_data, LOG_inflated_data = parse()
-find_pixels_shifts(PIR_data, [962,990,0,191], center_cc=False, time_cc=False)
+#find_pixels_shifts(PIR_data, [1050,1070,0,191], center_cc=False, time_cc=False)
+find_pixels_shifts(PIR_data, [1350,1570,0,191], center_cc=False, time_cc=False)
 
