06af57001642ba8987fd4e33da95efd00769a365
Fangyu Wu <fangyuthegreen@gmail.com>
2015-12-02 14:20:00 -0600

    Added modified speed estimation method.

--- speed_estimation.py
+++ speed_estimation.py
@@ -30,21 +30,25 @@ transform_right = [np.tan(angle) for angle in transform+center_right]
 distance_book = np.concatenate((transform_left, transform_mid, transform_right))
 
 # Background subtraction by pixel median
-def background_subtraction(_PIR_data):
+def background_subtraction(PIR_data):
     #print "Running background subtraction"
     #plt.figure()
-    #plt.imshow(_PIR_data)
+    #plt.imshow(PIR_data)
     median = []
-    for i in range(len(_PIR_data[0])):
-        median.append(np.median([t[i] for t in _PIR_data]))
+    for i in range(len(PIR_data[0])):
+        median.append(np.median([t[i] for t in PIR_data]))
     median = np.array(median)
-    _PIR = []
-    for data in _PIR_data:
-        _PIR.append(np.array(data)-median)
+    #stdev = []
+    #for i in range(len(PIR_data[0])):
+    #    stdev.append(np.std([t[i] for t in PIR_data]))
+    #stdev = np.array(stdev)
+    PIR = []
+    for data in PIR_data:
+        PIR.append(np.array(data)-median)
     #plt.figure()
     #plt.imshow(_PIR)
     #plt.show()
-    return _PIR
+    return PIR
 
 
 def find_slope(PIR_data, begin, end, distance, display=False):
@@ -67,30 +71,60 @@ def find_slope(PIR_data, begin, end, distance, display=False):
     Pixel = []
     t = 0
     # This assumes that vehicles are hotter than ambient temperature
-    threshold = np.percentile(passing_window, 95)
-    for sample, left_sample, right_sample in zip(passing_window[1:-1], 
-                                                 passing_window[:-1],
-                                                 passing_window[1:]):
-        for idx in np.arange(3, len(sample)-3):
-            if (np.sum(sample[idx-3:idx+3]) + 
-                np.sum(left_sample[idx-1:idx+1]) + 
-                np.sum(right_sample[idx-1:idx+1])) > 6*threshold:
-                Time.append(t)
-                Pixel.append(distance_book[idx/4])
+    #threshold = np.percentile(passing_window, 85)
+    #for sample, left_sample, right_sample in zip(passing_window[1:-1], 
+    #                                             passing_window[:-1],
+    #                                             passing_window[1:]):
+    #    for idx in np.arange(3, len(sample)-3):
+    #        if (np.sum(sample[idx-3:idx+3]) + 
+    #            np.sum(left_sample[idx-1:idx+1]) + 
+    #            np.sum(right_sample[idx-1:idx+1])) > 6*threshold:
+    #            Time.append(t)
+    #            Pixel.append(distance_book[idx/4])
+    #            #Pixel.append(idx)
+    #    t += 1
+    
+    threshold = np.percentile(passing_window, 80)
+    for sample in passing_window:
+        for idx in np.arange(len(sample)):
+            if sample[idx] > threshold:
+                for iter in range(int(sample[idx])):
+                    Time.append(t + np.random.random())
+                    Pixel.append(distance_book[idx/4] * 
+                                 (1 + 0.05*np.random.random()))
         t += 1
+
     Time = np.array(Time).reshape((len(Time),1))
     Pixel = np.array(Pixel).reshape((len(Pixel),1))
     
     # Robustly fit the linear model with RANSAC algorithm
-    ransac_model = linear_model.RANSACRegressor(linear_model.LinearRegression())
+    ransac_model = linear_model.RANSACRegressor(linear_model.LinearRegression(), 
+                                                min_samples=3, max_trials=1000)
     ransac_model.fit(Time, Pixel)
     inlier_mask = ransac_model.inlier_mask_
     outlier_mask = np.logical_not(inlier_mask)
-    estimated_speed = 23*ransac_model.estimator_.coef_[0][0]*distance
+
+    speed_coef = 23
+    estimated_speed = speed_coef*ransac_model.estimator_.coef_[0][0]*distance
 
     # Prediction based on ransac algorithm
-    ransac_Pixel = ransac_model.predict(Time)
-    
+    #ransac_Pixel = ransac_model.predict(Time)
+
+    # Sensitivity analysis
+    slope = ransac_model.estimator_.coef_[0][0]
+    intercept = ransac_model.estimator_.intercept_[0]
+    ransac_Pixel = Time*slope + intercept
+    # 5 mph higher than the estimate
+    slope_higher = (estimated_speed + 5) / (speed_coef*distance)
+    ransac_Pixel_higher = Time*slope_higher + intercept
+    shift_higher = np.median(ransac_Pixel) - np.median(ransac_Pixel_higher)
+    ransac_Pixel_higher += shift_higher
+    # 5 mph lower than the estimate
+    slope_lower = (estimated_speed - 5) / (speed_coef*distance)
+    ransac_Pixel_lower = Time*slope_lower + intercept
+    shift_lower = np.median(ransac_Pixel_lower) - np.median(ransac_Pixel)
+    ransac_Pixel_lower -= shift_lower
+
     if False:
         # Experiment with edge detection
         edge_roberts = roberts(passing_window)
@@ -118,15 +152,16 @@ def find_slope(PIR_data, begin, end, distance, display=False):
 
         fig = plt.figure(figsize=(15, 5), dpi=100)
         ax0 = fig.add_subplot(121, projection="3d")
-        ax0.plot_surface(topo_X, topo_Y, topo_Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-                         linewidth=0, antialiased=False, vmin=-5, vmax=20)
+        ax0.contourf(topo_X, topo_Y, topo_Z, 100, vmin=-5, vmax=20)
+        ax0.plot(Time, ransac_Pixel, 22, 'r')
+        #ax0.plot_surface(topo_X, topo_Y, topo_Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+        #                 linewidth=0, antialiased=False, vmin=-5, vmax=20)
         ax0.set_xlabel("Time (0.125 sec)")
         ax0.set_ylabel("Norminal Distance from Centerline")
         ax0.set_zlabel("Nominal Temperature")
         ax0.set_title("Measured Distance = %.2f m" % distance)
 
         ax = fig.add_subplot(122)
-        ax.contourf(topo_X, topo_Y, topo_Z, 100, vmin=-5, vmax=20)
         ax.set_xlim(0, passing_window.shape[0]-1)
         ax.set_ylim(-np.tan(2*np.pi/5)+0.3, np.tan(2*np.pi/5)-0.3)
         ax.set_xlabel("Time (0.125 sec)")
@@ -134,9 +169,13 @@ def find_slope(PIR_data, begin, end, distance, display=False):
         ax.set_title("Estimated Slope = %.2f /sec Estimated Speed = %.2f mph" 
                      % (ransac_model.estimator_.coef_[0][0], estimated_speed))
         
-        ax.scatter(Time[inlier_mask], Pixel[inlier_mask], color='g', label="Inliers")
-        ax.scatter(Time[outlier_mask], Pixel[outlier_mask], color='r', label="Outliers")
+        ax.scatter(Time[inlier_mask], Pixel[inlier_mask], 
+                   marker='.', color='g', label="Inliers")
+        ax.scatter(Time[outlier_mask], Pixel[outlier_mask], 
+                   marker='.', color='r', label="Outliers")
         ax.plot(Time, ransac_Pixel, 'c', label="Robust LR", linewidth=1.5)
+        ax.plot(Time, ransac_Pixel_higher, '--r', label="Robust LR High", linewidth=1.5)
+        ax.plot(Time, ransac_Pixel_lower, '--b', label="Robust LR Low", linewidth=1.5)
         #ax.legend(loc="lower right")
         
         plt.show()
